23eac9f4b89e15b015212fd80ce6c4916f5ecf22
biingen <biingen@gmail.com>
2019-11-26 10:34:17 +0800

    Change build name to PSC.exe

--- PSC/Properties/Resources.Designer.cs
+++ PSC/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DDS.Properties {
+namespace PSC.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DDS.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DDS.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PSC.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
